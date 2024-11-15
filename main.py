7b5d0640d102e3bde0064b43cb1f4f8411b2c84a
Sebastian-O-dev <sebastianolechno@gmail.com>
2024-11-15 18:59:28 +0100

    Update main.py
    fixed many functional issues, redundancies and logical contradictions/infinite loops

--- main.py
+++ main.py
@@ -1,61 +1,67 @@
 import random
-
-import hangman_assets
+import hangman_assets as ha
 
 lives = 6
-
-print(hangman_assets.logo)
-
-chosen_word = random.choice(hangman_assets.word_list)
-# print(chosen_word)
-
 placeholder = ""
-
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
-
-print(hangman_assets.stages[lives])
-print("\nWord to guess -> " + placeholder + "   (each underscore is a letter)")
-
+chosen_word = random.choice(ha.word_list)
+# print(chosen_word)
 game_over = False
 correct_letters = []
+wrong_letters = []
+for position in chosen_word:
+    placeholder += "_"
+
+print(ha.logo)
+
+print(f"\nThe gallows have been prepared...\n{ha.stages[lives]}")
+
+print(f"\nWord to guess -> \"{placeholder}\"   (each underscore is a letter)")
 
 while not game_over:
 
-    print(f"\n*************************** {lives}/6 LIVES LEFT ***************************")
-    guess = input("Guess a letter: ").lower()
-
     display = ""
 
+    print(f"\n*************************** {lives}/6 LIVES LEFT ***************************")
+    guess = input("\nGuess a letter: ").lower()
+
+# display variable block
     for letter in chosen_word:
-        if letter == guess:
+        if guess == letter:
             display += letter
-            if guess in correct_letters:
-                print(f"You've already guessed {guess}")
-            else:
-                correct_letters.append(guess)
         elif letter in correct_letters:
             display += letter
         else:
             display += "_"
 
-    print("\nWord to guess: " + display)
+# correct and wrong letter list assignment block
+    if guess in chosen_word:
+        if guess not in correct_letters:
+            correct_letters.append(guess)
+            print(f"Good guess, the letter \"{guess}\", is in the word.")
+        else:
+            print(f"You've already guessed the letter \"{guess}\", try another letter.")
+    else:
+        if guess not in wrong_letters:
+            wrong_letters.append(guess)
+            lives -= 1
+            print(f"\nYou guessed the letter \"{guess}\", that's not in the word. You lose a life.")
+        else:
+            print(f"You've already guessed the letter \"{guess}\", try another.")
 
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"\nYou guessed the letter \"{guess}\", that's not in the word. You lose a life.")
+    print(f"\nWord you're guessing: {display}\n")
 
-        if lives == 0:
-            game_over = True
-
-            print("***************************** YOU LOSE ********************************")
-            print(f"                   THE WORD WAS {chosen_word}!")
+# "lives broker" block
+    if lives == 0:
+        game_over = True
+        print("***************************** YOU LOSE ********************************")
+        print(f"                   THE WORD WAS \"{chosen_word}\"!")
 
     if "_" not in display:
         game_over = True
         print("***************************** YOU WIN ********************************")
-        print(f"                   THE WORD WAS {chosen_word}!")
+        print(f"                   THE WORD WAS \"{chosen_word}\"!")
 
-    print(f"You finished with {lives}/6, your 'hangman' below:\n", hangman_assets.stages[lives])
-  
+    print(ha.stages[lives])
+
+# final message
+print(f"You finished with {lives}/6 lives, your 'hangman' ^^^.\n", ha.stages[lives])
